986d438dddc747ec1a0d7b0ebe1ac189bbc71664
Eren <eren.livingstone@gmail.com>
2024-02-06 14:36:32 -0500

    Add death zone and ball life lost

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -10,11 +10,13 @@ var start_position: Vector2
 
 @export var ball_speed = 20
 @export var lives = 3
+@export var death_zone: DeathZone
 
 @onready var collision_shape_2d = $CollisionShape2D
 
 func _ready():
 	start_position = position
+	death_zone.life_lost.connect(on_life_lost)
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * ball_speed * delta)
@@ -29,3 +31,14 @@ func start_ball():
 	
 	# Set a random Vector2 to fire the ball in an upwards direction
 	velocity = Vector2(randf_range(-1, 1), randf_range(-.1, -1)).normalized() * ball_speed
+
+func reset_ball():
+	position = start_position
+	velocity = Vector2.ZERO
+
+func on_life_lost():
+	lives -= 1
+	if lives == 0:
+		pass
+	else:
+		reset_ball()
---
+++ scripts/death_zone.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+
+class_name DeathZone
+
+signal life_lost
+
+func _on_body_entered(body):
+	life_lost.emit()
